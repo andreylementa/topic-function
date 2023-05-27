c0142abb32d524739abf9baeb2b46365578b72b6
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-05-27 11:44:52 +0300

    Call and Apply

--- script.js
+++ script.js
@@ -1,68 +1,68 @@
-'use strict';
+//'use strict';
 
-// Default Parameters
+//// Default Parameters
 
-const bookings = [];
+//const bookings = [];
 
-const makeBooking = function (
-  flightNum,
-  passengersNum = 1,
-  price = 99 * passengersNum
-) {
-  // Before ES6
-  // passengersNum = passengersNum || 1;
-  // price = price || 99;
+//const makeBooking = function (
+//  flightNum,
+//  passengersNum = 1,
+//  price = 99 * passengersNum
+//) {
+//  // Before ES6
+//  // passengersNum = passengersNum || 1;
+//  // price = price || 99;
 
-  const booking = {
-    flightNum,
-    passengersNum,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
-};
+//  const booking = {
+//    flightNum,
+//    passengersNum,
+//    price,
+//  };
+//  console.log(booking);
+//  bookings.push(booking);
+//};
 
-makeBooking('QE123');
-makeBooking('TU235', 3, 390);
-makeBooking('QE123', 1);
-makeBooking('QE123', 4);
-makeBooking('QE123', undefined, 400);
+//makeBooking('QE123');
+//makeBooking('TU235', 3, 390);
+//makeBooking('QE123', 1);
+//makeBooking('QE123', 4);
+//makeBooking('QE123', undefined, 400);
 
-// Passing arguments. Values vs Reference
+//// Passing arguments. Values vs Reference
 
-const flightNumber = 'BV328';
-const passenger1145 = {
-  firstName: 'Jack',
-  lastName: 'Brown',
-  passport: 128490,
-};
+//const flightNumber = 'BV328';
+//const passenger1145 = {
+//  firstName: 'Jack',
+//  lastName: 'Brown',
+//  passport: 128490,
+//};
 
-const checkIn = function (flight, passenger) {
-  flight = 'bv328';
-  passenger.firstName = passenger.firstName.toLowerCase();
-  passenger.lastName = passenger.lastName.toLowerCase();
+//const checkIn = function (flight, passenger) {
+//  flight = 'bv328';
+//  passenger.firstName = passenger.firstName.toLowerCase();
+//  passenger.lastName = passenger.lastName.toLowerCase();
 
-  if (passenger.passport === 128490) {
-    alert('Welcome to the board!');
-  } else {
-    alert('Incorrect passport!');
-  }
-};
+//  if (passenger.passport === 128490) {
+//    alert('Welcome to the board!');
+//  } else {
+//    alert('Incorrect passport!');
+//  }
+//};
 
-checkIn(flightNumber, passenger1145);
-console.log(flightNumber);
-console.log(passenger1145);
+//checkIn(flightNumber, passenger1145);
+//console.log(flightNumber);
+//console.log(passenger1145);
 
-// This is the same as doing it
-const flight = flightNumber;
-const passenger = passenger1145;
+//// This is the same as doing it
+//const flight = flightNumber;
+//const passenger = passenger1145;
 
-const changePassportNumber = function (person) {
-  person.passport = Math.trunc(Math.random() * 1000000000);
-};
+//const changePassportNumber = function (person) {
+//  person.passport = Math.trunc(Math.random() * 1000000000);
+//};
 
-changePassportNumber(passenger1145);
-checkIn(flightNumber, passenger1145);
+//changePassportNumber(passenger1145);
+//checkIn(flightNumber, passenger1145);
 
 // Функции Принимающие Callback Функции
 
@@ -110,3 +110,59 @@ const arrGreet = greetingText => name =>
   console.log(`${greetingText} ${name}!`);
 
 arrGreet('Hello')('Lilu');
+
+// Методы call(), apply(), bind()
+
+const book = function (flightNumber, passengerName) {
+  console.log(
+    `${passengerName} has booked a ticket on ${this.airlineName} flight ${this.iataCode}${flightNumber}`
+  );
+  this.bookings.push({
+    flight: `${this.iataCode}${flightNumber}`,
+    passengerName,
+  });
+};
+
+const airline1 = {
+  airlineName: 'SkyUp',
+  iataCode: 'SU',
+  bookings: [],
+  //   book(flightNumber, passengerName) {
+  //     console.log(
+  //       `${passengerName} has booked a ticket on ${this.airlineName} flight ${this.iataCode}${flightNumber}`
+  //     );
+  //     this.bookings.push({
+  //       flight: `${this.iataCode}${flightNumber}`,
+  //       passengerName,
+  //     });
+  //   },
+};
+
+// airline1.book(346, 'Jim Green');
+// airline1.book(635, 'Michael Jordan');
+// console.log(airline1);
+
+const airline2 = {
+  airlineName: 'EuroFlights',
+  iataCode: 'EF',
+  bookings: [],
+};
+
+// const book = airline1.book;
+
+// This doesn't work
+// book(345, 'Linda Wilams');
+
+// call()
+book.call(airline2, 345, 'Linda Wilams');
+console.log(airline2);
+
+book.call(airline1, 111, 'Bob Smith');
+console.log(airline1);
+
+// apply() - old approach
+const flightData = [111, 'Nick Wong'];
+// book.apply(airline2, flightData);
+
+book.call(airline2, ...flightData);
+console.log(airline2);
